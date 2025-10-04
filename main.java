0ee0e0258eb1b7ddf38df37d5b5998892c0e08da
abhishektehriya <abhisheksharma09072004@gmail.com>
2025-10-04 19:03:07 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("===== Employee Sorting Program =====");
+        SortEmployees.main(args);
+
+        System.out.println("\n===== Student Filtering & Sorting Program =====");
+        FilterAndSortStudents.main(args);
+
+        System.out.println("\n===== Product Analytics Program =====");
+        ProductAnalytics.main(args);
+    }
+}
